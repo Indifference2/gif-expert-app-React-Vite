2c44e31723282b5c84822eca807989ec85484ec4
Ivan <ezequielivan2@gmail.com>
2024-03-11 17:26:51 -0300

    <img> alt change to dynamic

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const GifItem = ({title, url}) => {
   return (
     <div className="card">
-        <img src={url} alt="title" />
+        <img src={url} alt={title} />
         <p>{title}</p>
     </div>
     
